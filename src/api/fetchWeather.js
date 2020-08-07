c353d5d34a15f23417ef26a9dc34381c41650dbc
Darragh Ferry <darraghferry@hotmail.com>
2020-08-07 19:30:02 +0100

    added css file and code

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "a6360d8d47aeba5423fa0972235c1e83";
+
+export const fetchWeather = async query => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY
+    }
+  });
+  return data;
+};
